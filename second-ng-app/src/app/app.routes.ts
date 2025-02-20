c02455d3a3a235f9303fedc8a1ac807265e2e9f0
Maja Domalewska <162155232+majka-domalewska@users.noreply.github.com>
2025-02-20 14:28:33 +0000

    learning about routing

--- second-ng-app/src/app/app.routes.ts
+++ second-ng-app/src/app/app.routes.ts
@@ -1,5 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [
-
-];
+export const routes: Routes = [{
+        path: '',
+        pathMatch: 'full',
+        loadComponent: () => 
+        import('./home/home.component').then(m => m.HomeComponent)
+    }, {
+        path: 'todos',   
+        pathMatch: 'full',
+        loadComponent: () =>
+        import('./todos/todos.component').then(m => m.TodosComponent)
+    }];
